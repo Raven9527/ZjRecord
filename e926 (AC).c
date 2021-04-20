549472b2d326ba4a94fcf829efbf8d3bbe1286d6
Hayato <acd667y@gmail.com>
2021-04-21 04:23:32 +0800

    e926

---
+++ e926 (AC).c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+int main()
+{
+    printf("|\'o\'|\n");
+    printf("\\^_^/\n");
+    printf("(\"o\")\n");
+    printf("[-%%-]");
+    return 0;
+}
